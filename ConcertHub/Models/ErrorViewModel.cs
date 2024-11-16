0cb2938025f6f5adfd3496de9e32d954ff5566dd
MartinManchev03 <martinmanchev162003@abv.bg>
2024-11-16 20:40:26 +0200

    Add project files.

---
+++ ConcertHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ConcertHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
