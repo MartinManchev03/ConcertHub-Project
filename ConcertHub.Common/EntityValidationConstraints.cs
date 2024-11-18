17d6d61a7823b3a9fe5e1e936524f5cf195f9f38
MartinManchev03 <martinmanchev162003@abv.bg>
2024-11-18 14:53:15 +0200

    Add ConcertConfiguration and validation constraints for common

---
+++ ConcertHub.Common/EntityValidationConstraints.cs
@@ -0,0 +1,16 @@
+﻿namespace ConcertHub.Common
+{
+    public static class EntityValidationConstraints
+    {
+        public static class ConcertValidation
+        {
+            public static int ConcertNameMaxLenght = 100;
+            public static int ConcertNameMinLenght = 3;
+            public static int DescriptionMaxLenght = 500;
+            public static int DescriptionMinLenght = 10;
+            public static int LocationMaxLenght = 100;
+            public static int LocationMinLenght = 5;
+
+        }
+    }
+}
---
+++ ConcertHub.Data/Configurations/ConcertConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using ConcertHub.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConcertHub.Common.EntityValidationConstraints.ConcertValidation;
+namespace ConcertHub.Data.Configurations
+{
+    public class ConcertConfiguration : IEntityTypeConfiguration<Concert>
+    {
+        public void Configure(EntityTypeBuilder<Concert> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.ConcertName)
+                .IsRequired()
+                .HasMaxLength(ConcertNameMaxLenght);
+
+            builder.Property(c => c.Description)
+                .IsRequired()
+                .HasMaxLength(DescriptionMaxLenght);
+
+            builder.Property(c => c.StartDate)
+                .IsRequired();
+
+            builder.Property(c => c.EndDate)
+                .IsRequired();
+
+            builder.Property(c => c.Location)
+               .IsRequired()
+               .HasMaxLength(LocationMaxLenght);
+
+            builder.HasOne(c => c.Organizer)
+                .WithMany()
+                .HasForeignKey(c => c.OrganizerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(c => c.Category)
+                .WithMany()
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
