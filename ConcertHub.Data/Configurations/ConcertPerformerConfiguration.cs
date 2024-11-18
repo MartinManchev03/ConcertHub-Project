bdc401cee111a06bb81d0b983bbc2d7f4dcbc800
MartinManchev03 <martinmanchev162003@abv.bg>
2024-11-18 15:08:39 +0200

    Add concertperformer configuration

---
+++ ConcertHub.Data/Configurations/ConcertPerformerConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using ConcertHub.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcertHub.Data.Configurations
+{
+    public class ConcertPerformerConfiguration : IEntityTypeConfiguration<ConcertPerformer>
+    {
+        public void Configure(EntityTypeBuilder<ConcertPerformer> builder)
+        {
+            builder.HasKey(cp => new { cp.PerformerId, cp.ConcertId });
+
+            builder.HasOne(c => c.Concert)
+                .WithMany(cp => cp.ConcertsPerformers)
+                .HasForeignKey(c => c.ConcertId);
+
+            builder.HasOne(p => p.Performer)
+                .WithMany(cp => cp.ConcertsPerformers)
+                .HasForeignKey(p => p.PerformerId);
+        }
+    }
+}
