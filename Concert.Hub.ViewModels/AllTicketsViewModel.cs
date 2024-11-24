0d2ce2377a7c0d0d1d6ee37aee5537b549ac06b3
MartinManchev03 <martinmanchev162003@abv.bg>
2024-11-24 16:11:35 +0200

    Add AllTicketsViewModel

---
+++ Concert.Hub.ViewModels/AllTicketsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using ConcertHub.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcertHub.ViewModels
+{
+    public class AllTicketsViewModel
+    {
+        public Guid Id { get; set; }
+
+        public TicketType TicketType { get; set; }
+
+        public string ConcertName { get; set; }
+    }
+}
