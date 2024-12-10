8cd3ab1e260e15101ef1c24a3426bca01e0f15ab
MartinManchev03 <martinmanchev162003@abv.bg>
2024-12-11 00:17:28 +0200

    Add error controller which controller error 403, 404 and 500 dynamically

--- ConcertHub/Controllers/ErrorController.cs
+++ ConcertHub/Controllers/ErrorController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ConcertHub.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConcertHub.Controllers
@@ -10,10 +11,30 @@ namespace ConcertHub.Controllers
             Response.StatusCode = 404;
             return View();
         }
-        public IActionResult Error(int statusCode, string message)
+        public IActionResult Error(string message)
         {
-            Response.StatusCode = statusCode;
-            return View(message);
+            var model = new ErrorViewModel();
+            if(message == "Error 404")
+            {
+                Response.StatusCode = 404;
+                model.StatusCodeTitle = "Error 404 - Page Not Found";
+                model.ErrorMessage = "The page you are looking for does not exist.";
+
+
+            }
+            else if(message == "Error 403")
+            {
+                Response.StatusCode = 403;
+                model.StatusCodeTitle = "Error 403 - Forbidden";
+                model.ErrorMessage = "You do not have permission to access this resource. Please check your credentials or contact your administrator if you believe this is an error.";
+            }
+            else
+            {
+                Response.StatusCode = 500;
+                model.StatusCodeTitle = "Error 500 - Internal Server Error";
+                model.ErrorMessage = "An unexpected error occurred. Please try again later.";
+            }
+            return View(model);
         }
     }
 }
